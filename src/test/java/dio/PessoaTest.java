5dd53e4aa4d7c0203513cf88d67c89c3e2f8dcdb
Eduardo Aguiar de Araujo <araujo.ea@gmail.com>
2024-05-17 13:16:13 -0300

    First commit

---
+++ src/test/java/dio/PessoaTest.java
@@ -0,0 +1,16 @@
+package dio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class PessoaTest {
+
+    @Test
+    void validarCalculoIdade(){
+        Pessoa pessoa = new Pessoa("Julia", LocalDateTime.of(2020,1,1, 15, 0, 0));
+        Assertions.assertEquals(4, pessoa.getIdade());
+    }
+}
